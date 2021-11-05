e1ce6b2fa1f397517241a32082b84c89ec484fd9
Arturo <ac@iconstorm.com>
2021-11-05 14:32:40 +0100

    chore: format (prettier)

--- temp/tokens-mode-attr/build.js
+++ temp/tokens-mode-attr/build.js
@@ -1,37 +1,37 @@
-const StyleDictionary = require('style-dictionary')
+const StyleDictionary = require('style-dictionary');
 
-const PREFIX = 'scl'
-const CSS_OUTPUT_PATH = 'temp/tokens-mode-attr/build/'
-const SOURCE_PATH = 'temp/tokens-mode-attr/'
+const PREFIX = 'scl';
+const CSS_OUTPUT_PATH = 'temp/tokens-mode-attr/build/';
+const SOURCE_PATH = 'temp/tokens-mode-attr/';
 
 function modeFormatWrapper(mode, format) {
   return function (args) {
-    const dictionary = Object.assign({}, args.dictionary)
-    // FIXME using `allProperties` here renders the references, 
+    const dictionary = Object.assign({}, args.dictionary);
+    // FIXME using `allProperties` here renders the references,
     // but the values are incorrect :(
-    dictionary.allTokens = dictionary.allTokens.map(token => {
+    dictionary.allTokens = dictionary.allTokens.map((token) => {
       if (token.mode && token.mode[mode]) {
-        return Object.assign({}, token, { value: token.mode[mode ]})
+        return Object.assign({}, token, { value: token.mode[mode] });
       } else {
-        return token
+        return token;
       }
-    })
-    return StyleDictionary.format[format]({ ...args, dictionary })
-  }
+    });
+    return StyleDictionary.format[format]({ ...args, dictionary });
+  };
 }
 
 /**
  * Testing handling light and dark modes with attributes
- * 
+ *
  * for CSS
  * -------
- * Builds a variables.css in light mode, including core tokens, 
+ * Builds a variables.css in light mode, including core tokens,
  * and a variables.dark.css with only the dark mode overwrites.
- * 
+ *
  * for Sketch
  * ----------
  * TODO
- * 
+ *
  * for Figma
  * ---------
  * TODO
@@ -39,35 +39,37 @@ function modeFormatWrapper(mode, format) {
 
 StyleDictionary.extend({
   format: {
-    'dark:css/variables': modeFormatWrapper('dark', 'css/variables')
+    'dark:css/variables': modeFormatWrapper('dark', 'css/variables'),
   },
   source: [
     SOURCE_PATH + '/core/**/*.json',
     SOURCE_PATH + '/system/**/*.json',
-    SOURCE_PATH + '/components/**/*.json'
+    SOURCE_PATH + '/components/**/*.json',
   ],
   platforms: {
     css: {
       transformGroup: 'css',
       prefix: PREFIX,
       buildPath: CSS_OUTPUT_PATH + 'css/',
-      files: [{
-        destination: 'variables.css',
-        format: 'css/variables',
-        options: {
-          outputReferences: true,
-          selector: ':root, [data-mode="light"]'
-        }
-      },
-      {
-        destination: 'variables.dark.css',
-        format: 'dark:css/variables',
-        filter: (token) => token.mode != null && token.mode.dark != null,
-        options: {
-          outputReferences: true, // FIXME this is not being respected
-          selector: '[data-mode="dark"]'
-        }
-      }]
-    }
-  }
-}).buildAllPlatforms()
+      files: [
+        {
+          destination: 'variables.css',
+          format: 'css/variables',
+          options: {
+            outputReferences: true,
+            selector: ':root, [data-mode="light"]',
+          },
+        },
+        {
+          destination: 'variables.dark.css',
+          format: 'dark:css/variables',
+          filter: (token) => token.mode != null && token.mode.dark != null,
+          options: {
+            outputReferences: true, // FIXME this is not being respected
+            selector: '[data-mode="dark"]',
+          },
+        },
+      ],
+    },
+  },
+}).buildAllPlatforms();
--- temp/tokens-mode-file/build.js
+++ temp/tokens-mode-file/build.js
@@ -1,22 +1,22 @@
-const fs = require('fs/promises')
-const StyleDictionary = require('style-dictionary')
+const fs = require('fs/promises');
+const StyleDictionary = require('style-dictionary');
 
-const PREFIX = 'scl'
-const CSS_OUTPUT_PATH = 'temp/tokens-mode-file/build/'
-const SOURCE_PATH = 'temp/tokens-mode-file/'
+const PREFIX = 'scl';
+const CSS_OUTPUT_PATH = 'temp/tokens-mode-file/build/';
+const SOURCE_PATH = 'temp/tokens-mode-file/';
 
 /**
  * Testing handling light and dark modes with filenames (foo.light.json)
- * 
+ *
  * for CSS
  * -------
- * Builds a variables.css in light mode, including core tokens, 
+ * Builds a variables.css in light mode, including core tokens,
  * and a variables.dark.css with only the dark mode overwrites.
- * 
+ *
  * for Sketch
  * ----------
  * TODO
- * 
+ *
  * for Figma
  * ---------
  * TODO
@@ -24,18 +24,18 @@ const SOURCE_PATH = 'temp/tokens-mode-file/'
 
 StyleDictionary.registerAction({
   name: 'bundle_css',
-  do: async function(dictionary, config) {
+  do: async function (dictionary, config) {
     // TODO get these from `config`?
-    const COMMON_PATH = CSS_OUTPUT_PATH + 'css/'
-    const light = await fs.readFile(COMMON_PATH + 'variables.css')
-    const dark = await fs.readFile(COMMON_PATH + 'variables.dark.css')
-    await fs.writeFile(COMMON_PATH + 'variables.all.css', light + '\n' + dark)
+    const COMMON_PATH = CSS_OUTPUT_PATH + 'css/';
+    const light = await fs.readFile(COMMON_PATH + 'variables.css');
+    const dark = await fs.readFile(COMMON_PATH + 'variables.dark.css');
+    await fs.writeFile(COMMON_PATH + 'variables.all.css', light + '\n' + dark);
   },
-  undo: async function() {
-    const COMMON_PATH = CSS_OUTPUT_PATH + 'css/'
-    await fs.rm(COMMON_PATH + 'variables.all.css')
-  }
-})
+  undo: async function () {
+    const COMMON_PATH = CSS_OUTPUT_PATH + 'css/';
+    await fs.rm(COMMON_PATH + 'variables.all.css');
+  },
+});
 
 // CSS light mode (default)
 StyleDictionary.extend({
@@ -44,54 +44,56 @@ StyleDictionary.extend({
     SOURCE_PATH + 'system/system.common.json',
     SOURCE_PATH + `system/system.light.json`,
     SOURCE_PATH + 'components/**/*.common.json',
-    SOURCE_PATH + 'components/**/*.light.json'
+    SOURCE_PATH + 'components/**/*.light.json',
   ],
   platforms: {
     css: {
       transformGroup: 'css',
       prefix: PREFIX,
       buildPath: CSS_OUTPUT_PATH + 'css/',
-      files: [{
-        destination: 'variables.css',
-        format: 'css/variables'
-      }],
+      files: [
+        {
+          destination: 'variables.css',
+          format: 'css/variables',
+        },
+      ],
       options: {
         outputReferences: true,
-        selector: ':root, [data-mode="light"]'
-      }
-    }
-  }
-}).buildAllPlatforms()
+        selector: ':root, [data-mode="light"]',
+      },
+    },
+  },
+}).buildAllPlatforms();
 
 // CSS dark mode
 StyleDictionary.extend({
-  include: [
-    SOURCE_PATH + 'system/system.light.json',
-  ],
+  include: [SOURCE_PATH + 'system/system.light.json'],
   source: [
     SOURCE_PATH + 'core/**.json',
     SOURCE_PATH + 'system/system.common.json',
     SOURCE_PATH + `system/system.dark.json`,
-    SOURCE_PATH + 'components/**/*.dark.json'
+    SOURCE_PATH + 'components/**/*.dark.json',
   ],
   platforms: {
     css: {
       transformGroup: 'css',
       prefix: PREFIX,
       buildPath: CSS_OUTPUT_PATH + 'css/',
-      files: [{
-        destination: 'variables.dark.css',
-        format: 'css/variables',
-        // Filter out `core` and `light` from output
-        filter: function (token) {
-          return token.filePath.indexOf('dark.json') > -1
-        }
-      }],
+      files: [
+        {
+          destination: 'variables.dark.css',
+          format: 'css/variables',
+          // Filter out `core` and `light` from output
+          filter: function (token) {
+            return token.filePath.indexOf('dark.json') > -1;
+          },
+        },
+      ],
       options: {
         outputReferences: true,
-        selector: '[data-mode="dark"]'
+        selector: '[data-mode="dark"]',
       },
-      actions: ['bundle_css']
-    }
-  }
-}).buildAllPlatforms()
+      actions: ['bundle_css'],
+    },
+  },
+}).buildAllPlatforms();
