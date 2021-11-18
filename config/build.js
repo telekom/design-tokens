fe88d0dd7fc6a42b0d1cadc38657358ff3ffa11b
Arturo Castillo Delgado <ac@iconstorm.com>
2021-11-18 10:25:43 -0600

    refactor: improve names in Figma JSON output (#61)

--- config/build.js
+++ config/build.js
@@ -2,10 +2,14 @@ const fs = require('fs/promises');
 const deep = require('deep-get-set');
 const prettier = require('prettier');
 const StyleDictionary = require('style-dictionary');
+const { upperFirst } = require('style-dictionary/lib/utils/es6_');
 
 const PREFIX = 'scl';
 const OUTPUT_PATH = 'build/';
 const SOURCE_PATH = 'src/';
+
+const FIGMA_KEY_LIGHT = 'Light';
+const FIGMA_KEY_DARK = 'Dark';
 
 /**
  * This is WIP, we'll split it out in different files as we make progress.
@@ -34,29 +38,26 @@ StyleDictionary.registerFormat({
   },
 });
 
-const transformGroupFigma = [
-  'attribute/cti',
-  'name/cti/pascal', // TODO something custom
-  'size/px',
-  'color/hex',
-];
+const transformsFigma = ['attribute/cti', 'shadow/figma'];
 
 StyleDictionary.registerFormat({
   name: 'json/figma',
   formatter: function ({ dictionary }) {
-    const output = dictionary.tokens.semantic;
-    return JSON.stringify(minifyDictionary2(output), null, 2);
+    const output = formatJSON(dictionary.allTokens);
+    return JSON.stringify(output, null, 2);
   },
 });
 
 StyleDictionary.registerFormat({
-  name: 'json/figma-color',
+  name: 'json/figma-mode',
   formatter: function ({ dictionary }) {
-    const output = {
-      ...dictionary.tokens.semantic.color,
-      ...dictionary.tokens.semantic.elevation,
-    };
-    return JSON.stringify(minifyDictionary2(output), null, 2);
+    const output = formatJSON(
+      dictionary.allTokens.filter(
+        (token) =>
+          token.path.includes('color') || token.path.includes('elevation')
+      )
+    );
+    return JSON.stringify(output, null, 2);
   },
 });
 
@@ -80,10 +81,10 @@ StyleDictionary.registerAction({
 });
 
 StyleDictionary.registerAction({
-  name: 'bundle_json',
+  name: 'bundle_figma',
   do: async function (dictionary, config) {
     // TODO get these from `config`?
-    const COMMON_PATH = OUTPUT_PATH + 'json/';
+    const COMMON_PATH = OUTPUT_PATH + 'figma/';
     const modeless = JSON.parse(
       await fs.readFile(COMMON_PATH + 'variables.modeless.json')
     );
@@ -94,8 +95,8 @@ StyleDictionary.registerAction({
       await fs.readFile(COMMON_PATH + 'variables.dark.json')
     );
     const output = {
-      light: { ...light },
-      dark: { ...dark },
+      [FIGMA_KEY_LIGHT]: { ...light },
+      [FIGMA_KEY_DARK]: { ...dark },
       ...modeless,
     };
     await fs.writeFile(
@@ -133,11 +134,27 @@ StyleDictionary.registerTransform({
   },
 });
 
+// This does nothing for now
+StyleDictionary.registerTransform({
+  type: 'value',
+  name: 'shadow/figma',
+  // TODO remove `elevation` in path check?
+  matcher: (token) =>
+    token.original.type === 'shadow' || token.path.includes('elevation'),
+  transformer: function (token) {
+    const { value } = token.original;
+    const transform = (x) => ({
+      ...x,
+    });
+    return Array.isArray(value) ? value.map(transform) : transform(value);
+  },
+});
+
 StyleDictionary.extend({
   source: [SOURCE_PATH + '**/*.json5'],
   platforms: {
     css: {
-      transforms: ['mode-light', ...StyleDictionary.transformGroup.css],
+      transforms: [...StyleDictionary.transformGroup.css],
       prefix: PREFIX,
       buildPath: OUTPUT_PATH + 'css/',
       files: [
@@ -188,9 +205,9 @@ StyleDictionary.extend({
         },
       ],
     },
-    jsonModeless: {
-      transforms: [...transformGroupFigma],
-      buildPath: OUTPUT_PATH + 'json/',
+    figmaModeless: {
+      transforms: [...transformsFigma],
+      buildPath: OUTPUT_PATH + 'figma/',
       files: [
         {
           destination: 'variables.modeless.json',
@@ -206,13 +223,13 @@ StyleDictionary.extend({
         },
       ],
     },
-    jsonLight: {
-      transforms: ['mode-light', ...transformGroupFigma],
-      buildPath: OUTPUT_PATH + 'json/',
+    figmaLight: {
+      transforms: ['mode-light', ...transformsFigma],
+      buildPath: OUTPUT_PATH + 'figma/',
       files: [
         {
           destination: 'variables.light.json',
-          format: 'json/figma-color',
+          format: 'json/figma-mode',
           filter: (token) => {
             return (
               token.path[0] === 'semantic' &&
@@ -225,13 +242,13 @@ StyleDictionary.extend({
         },
       ],
     },
-    jsonDark: {
-      transforms: ['mode-dark', ...transformGroupFigma],
-      buildPath: OUTPUT_PATH + 'json/',
+    figmaDark: {
+      transforms: ['mode-dark', ...transformsFigma],
+      buildPath: OUTPUT_PATH + 'figma/',
       files: [
         {
           destination: 'variables.dark.json',
-          format: 'json/figma-color',
+          format: 'json/figma-mode',
           filter: (token) => {
             return (
               token.path[0] === 'semantic' &&
@@ -243,35 +260,29 @@ StyleDictionary.extend({
           },
         },
       ],
-      actions: ['bundle_json'],
+      actions: ['bundle_figma'],
     },
   },
 }).buildAllPlatforms();
 
-/**
- * Modified version of `minifyDictionary` in formatHelpers module
- * that returns `{ value, type }` instead of just `value`
- * @param {Object} obj - The object to minify. You will most likely pass `dictionary.tokens` to it.
- * @returns {Object}
- */
-function minifyDictionary2(obj) {
-  if (typeof obj !== 'object' || Array.isArray(obj)) {
-    return obj;
-  }
-
-  let output = {};
-
-  if (obj.hasOwnProperty('value')) {
-    return {
-      value: obj.value,
-      type: obj.type,
-    };
-  } else {
-    for (const name in obj) {
-      if (obj.hasOwnProperty(name)) {
-        output[name] = minifyDictionary2(obj[name]);
-      }
-    }
-  }
+function formatJSON(allTokens, nameCaseFn = figmaCase) {
+  const output = {};
+  deep.p = true;
+  allTokens.forEach((token) => {
+    let path = token.path.map(nameCaseFn);
+    if (path[0] === 'Semantic') path.shift(); // TODO remove after removing `semantic` key in source
+    deep(output, path, { value: token.value, type: token.type });
+  });
   return output;
+}
+
+function figmaCase(str) {
+  return str
+    .toLowerCase()
+    .replace('Ui', 'UI')
+    .replace('&-', '& ') // weird edge case
+    .replace('-', ' ')
+    .split(/\s/)
+    .map(upperFirst)
+    .join(' ');
 }
