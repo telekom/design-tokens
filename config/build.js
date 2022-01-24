1a0ee27f307262015ac147cca96f1f171162cea7
Arturo Castillo Delgado <ac@iconstorm.com>
2022-01-24 10:03:29 +0100

    Update README (#106)

--- config/build.js
+++ config/build.js
@@ -1,3 +1,13 @@
+/**
+ * Telekom Design Tokens https://github.com/telekom/design-tokens
+ *
+ * Copyright (c) 2021 Lukas Oppermann and contributors, Deutsche Telekom AG
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 const StyleDictionary = require('style-dictionary');
 
 require('./shared'); // register common transforms
