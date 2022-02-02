48ab2bd166d0610a5d7617f3af410729d5c55b87
Lukas Oppermann <oppermann.lukas@gmail.com>
2022-02-02 13:07:45 +0100

    bump sparkle

--- config/sketch-library-action.js
+++ config/sketch-library-action.js
@@ -18,7 +18,7 @@ const { OUTPUT_PATH, OUTPUT_BASE_FILENAME } = process.env;
 
 const SKETCH_FIXTURE_BASENAME = 'design-tokens';
 
-const SPARKLE = 8; // TODO automate somehow
+const SPARKLE = 9; // TODO automate somehow
 
 _.templateSettings.interpolate = /{{([\s\S]+?)}}/g; // mustache delimiters
 
