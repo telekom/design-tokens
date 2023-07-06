66034e8c7e324bdd17c0bed31edf828271c6a62f
Arturo Castillo Delgado <ac@iconstorm.com>
2023-07-06 09:23:58 +0200

    Figma: add core tokens to output (#215)
    * Include core tokens in Figma output
    
    * Format
    
    * Add $themes in JSON output for Figma
    
    * Fix broken links in Figma
    
    The problem is that for Spacing and Radius tokens some names are
     duplicated between core and semantic layers.
    
    * Fix references with modes and handle alpha colors
    
    * Comment
    
    * Change files

--- config/figma.config.js
+++ config/figma.config.js
@@ -11,7 +11,7 @@
 const fs = require('fs-extra');
 const deep = require('deep-get-set');
 const StyleDictionary = require('style-dictionary');
-const { humanCase, fontWeightMap } = require('./shared');
+const { humanCase, fontWeightMap, isColorAlphaComposite } = require('./shared');
 
 const { OUTPUT_PATH, OUTPUT_BASE_FILENAME } = process.env;
 const WHITELABEL = process.env.WHITELABEL !== 'false';
@@ -19,6 +19,11 @@ const WHITELABEL = process.env.WHITELABEL !== 'false';
 const TMP_NAME = 'tokens';
 const FIGMA_KEY_LIGHT = 'Light';
 const FIGMA_KEY_DARK = 'Dark';
+
+const shouldHaveMode = (token) =>
+  token.path[0] === 'color' || token.path[0] === 'shadow';
+
+const hasMode = (token) => token.original.value?.light != null;
 
 /*
   TODO:
@@ -48,23 +53,79 @@ const categoryTypeMap = {
   font: 'fontFamilies',
 };
 
-function formatJSON(allTokens, nameCaseFn = humanCase) {
+const THEMES = [
+  {
+    id: '0efe74c81045c31912f3f25ceccbb79a4021d482',
+    name: 'Telekom',
+    $figmaStyleReferences: {},
+    selectedTokenSets: {
+      Core: 'enabled',
+    },
+    group: 'Core',
+  },
+  {
+    id: '7a9907de042ac3a05850bed823405e364006587c',
+    name: 'Values',
+    $figmaStyleReferences: {},
+    selectedTokenSets: {
+      Core: 'source',
+      Global: 'enabled',
+    },
+    group: 'Global',
+  },
+  {
+    id: 'e84d59b7aab9a411698c3781401bf695dc2d2952',
+    name: 'Light',
+    $figmaStyleReferences: {},
+    selectedTokenSets: {
+      Core: 'source',
+      Light: 'enabled',
+    },
+    group: 'Color',
+  },
+  {
+    id: 'e3dd70729694cefd558242ba5e81517f4bc0d626',
+    name: 'Dark',
+    $figmaStyleReferences: {},
+    selectedTokenSets: {
+      Core: 'source',
+      Dark: 'enabled',
+    },
+    group: 'Color',
+  },
+];
+
+function formatJSON(allTokens, { dictionary, mode }) {
   const output = {};
   deep.p = true;
   allTokens.forEach((token) => {
-    const path = token.path.map(nameCaseFn);
+    const path = token.path.map(humanCase);
     if (path.includes('Motion')) return;
-    if (path[0] === 'Color' || path[0] === 'Shadow' || path[0] === 'Typography')
+    // Keep `Core` in path to avoid collisions with same name tokens
+    const keepCoreInPathToAvoidCollisionsWithSameNameTokens =
+      path[0] === 'Core' && path[1] !== 'Spacing' && path[1] !== 'Radius';
+    if (
+      path[0] === 'Color' ||
+      path[0] === 'Shadow' ||
+      path[0] === 'Typography' ||
+      keepCoreInPathToAvoidCollisionsWithSameNameTokens
+    ) {
       path.shift();
-    deep(output, path, getJSONValue(token));
+    }
+    deep(output, path, getJSONValue(token, { dictionary, mode }));
   });
   return output;
 }
 
 /**
- * Handle some special cases
+ * Get the proper value for each token, including:
+ * - renaming the `comment` key to `description`
+ * - renaming types to meet Tokens Studio requirements
+ * - keep references as values, when appropriate
+ * - handle references using `alpha` (leveraging Tokens Studio color modifier)
  */
-function getJSONValue(token) {
+function getJSONValue(token, { dictionary, mode }) {
+  let value = token.value; // cloning would be nice (`structuredClone` is not supported in Node 16?)
   const attributes = {
     type: token.type,
   };
@@ -73,25 +134,25 @@ function getJSONValue(token) {
     attributes.description = token.comment;
   }
   // Set type
-  if (token.attributes.category === 'typography') {
-    if (token.attributes.type === 'font-size') {
+  if (token.path.includes('typography')) {
+    if (token.path.includes('font-size')) {
       attributes.type = 'fontSizes';
     }
-    if (token.attributes.type === 'font-family') {
+    if (token.path.includes('font-family')) {
       attributes.type = 'fontFamilies';
     }
-    if (token.attributes.type === 'font-weight') {
+    if (token.path.includes('font-weight')) {
       attributes.type = 'fontWeights';
     }
-    if (token.attributes.type === 'line-spacing') {
+    if (token.path.includes('line-spacing')) {
       attributes.type = 'lineHeights';
     }
-    if (token.attributes.type === 'letter-spacing') {
+    if (token.path.includes('letter-spacing')) {
       attributes.type = 'letterSpacing';
     }
-  } else if (token.type === 'textStyle') {
+  } else if (token.path.includes('textStyle')) {
     attributes.type = categoryTypeMap['textStyle'];
-  } else if (token.type === 'shadow') {
+  } else if (token.path.includes('shadow')) {
     attributes.type = categoryTypeMap['shadow'];
   } else if (token.path.includes('line-weight')) {
     attributes.type = categoryTypeMap['line-weight'];
@@ -103,8 +164,41 @@ function getJSONValue(token) {
     attributes.type = categoryTypeMap['spacing'];
   }
 
+  // Keep reference when appropriate e.g. `{Core.Color.Black}`
+  // (mode is important!)
+  if (dictionary.usesReference(token.original.value)) {
+    let refs = hasMode(token)
+      ? [
+          ...dictionary.getReferences(token.original.value.light),
+          ...dictionary.getReferences(token.original.value.dark),
+        ]
+      : dictionary.getReferences(token.original.value);
+    // ! "shadow" type core tokens result in refs.length === 0
+    // (color modifiers are not supported within shadow tokens)
+    if (refs.length > 0) {
+      let ref = refs[0];
+      if (typeof mode !== 'undefined' && refs.length === 2) {
+        ref = mode === 'light' ? refs[0] : refs[1];
+      }
+      // Handle `alpha` colors -> Tokens Studio color modifier Pro feature
+      if (hasMode(token) && isColorAlphaComposite(token.original.value[mode])) {
+        deep(attributes, ['$extensions', 'studio.tokens', 'modify'], {
+          type: 'alpha',
+          value: token.original.value[mode].alpha?.toString(),
+          space: 'hsl',
+        });
+      }
+      const path = ref.path.map(humanCase);
+      // Keep `Core` in path to avoid collisions with same name tokens
+      if (path[1] !== 'Spacing' && path[1] !== 'Radius') {
+        path.shift();
+      }
+      value = `{${path.join('.')}}`;
+    }
+  }
+
   return {
-    value: token.value,
+    value,
     ...attributes,
   };
 }
@@ -132,18 +226,19 @@ StyleDictionary.registerTransform({
 StyleDictionary.registerFormat({
   name: 'json/figma',
   formatter: function ({ dictionary }) {
-    const output = formatJSON(dictionary.allTokens);
+    const output = formatJSON(dictionary.allTokens, { dictionary });
     return JSON.stringify(output, null, 2);
   },
 });
 
 StyleDictionary.registerFormat({
   name: 'json/figma-mode',
-  formatter: function ({ dictionary }) {
+  formatter: function ({ dictionary, options }) {
     const output = formatJSON(
       dictionary.allTokens.filter(
         (token) => token.path.includes('color') || token.path.includes('shadow')
-      )
+      ),
+      { dictionary, mode: options.mode }
     );
     return JSON.stringify(output, null, 2);
   },
@@ -153,6 +248,9 @@ StyleDictionary.registerAction({
   name: 'bundle_figma',
   do: async function (_, config) {
     const { buildPath } = config;
+    const core = JSON.parse(
+      await fs.readFile(buildPath + TMP_NAME + '.core.json')
+    );
     const modeless = JSON.parse(
       await fs.readFile(buildPath + TMP_NAME + '.modeless.json')
     );
@@ -167,14 +265,17 @@ StyleDictionary.registerAction({
       buildPath + OUTPUT_BASE_FILENAME + '.all.json',
       JSON.stringify(
         {
+          Core: {
+            ...core,
+          },
           Global: {
             ...modeless,
           },
           [FIGMA_KEY_LIGHT]: { ...light },
           [FIGMA_KEY_DARK]: { ...dark },
-          $themes: [],
+          $themes: THEMES,
           $metadata: {
-            tokenSetOrder: ['Global', FIGMA_KEY_LIGHT, FIGMA_KEY_DARK],
+            tokenSetOrder: ['Core', 'Global', FIGMA_KEY_LIGHT, FIGMA_KEY_DARK],
           },
         },
         null,
@@ -204,6 +305,7 @@ StyleDictionary.registerAction({
       )
     );
 
+    await fs.remove(buildPath + TMP_NAME + '.core.json');
     await fs.remove(buildPath + TMP_NAME + '.modeless.json');
     await fs.remove(buildPath + TMP_NAME + '.light.json');
     await fs.remove(buildPath + TMP_NAME + '.dark.json');
@@ -213,9 +315,6 @@ StyleDictionary.registerAction({
   },
 });
 
-const hasMode = (token) =>
-  token.path[0] === 'color' || token.path[0] === 'shadow';
-
 module.exports = {
   include: ['src/core/**/*.json5'],
   source: [
@@ -223,6 +322,17 @@ module.exports = {
     'src/semantic/**/*.json5',
   ],
   platforms: {
+    figmaCore: {
+      transforms: [...figmaTransformGroup],
+      buildPath: OUTPUT_PATH + 'figma/',
+      files: [
+        {
+          destination: TMP_NAME + '.core.json',
+          format: 'json/figma',
+          filter: (token) => token.path[0] === 'core',
+        },
+      ],
+    },
     figmaModeless: {
       transforms: [...figmaTransformGroup],
       buildPath: OUTPUT_PATH + 'figma/',
@@ -233,7 +343,7 @@ module.exports = {
           filter: (token) =>
             token.path[0] !== 'core' &&
             token.path[0] !== 'motion' &&
-            !hasMode(token),
+            !shouldHaveMode(token),
         },
       ],
     },
@@ -244,7 +354,10 @@ module.exports = {
         {
           destination: TMP_NAME + '.light.json',
           format: 'json/figma-mode',
-          filter: hasMode,
+          filter: shouldHaveMode,
+          options: {
+            mode: 'light',
+          },
         },
       ],
     },
@@ -255,7 +368,10 @@ module.exports = {
         {
           destination: TMP_NAME + '.dark.json',
           format: 'json/figma-mode',
-          filter: hasMode,
+          filter: shouldHaveMode,
+          options: {
+            mode: 'dark',
+          },
         },
       ],
       actions: ['bundle_figma'],
